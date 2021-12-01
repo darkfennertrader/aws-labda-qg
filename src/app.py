00d7739c06252927546d34b747232a05b604cbb4
Raimondo Marino <darkfenner69@gmail.com>
2021-12-01 07:29:21 +0100

    first release

--- src/app.py
+++ src/app.py
@@ -1,14 +1,20 @@
 import json
-from config import Config
+from config import Config, onnx_model_init, generate_question
 
+qg_model, qg_tokenizer = onnx_model_init(qg_model)
 
 def lambda_handler(event, context):
-    # TODO implementation
+    
     header = {"Content-Type": "application/json"}
     payload = {"message": "Lambda container image invoked!", "event": event}
+    
+    user_utterance = event["user_utterance"]
+    
+    query = generate_question(user_utterance, qg_model, qg_tokenizer)
 
     return {
         "header": header,
-        "statusCode": 200,
         "body": json.dumps(payload),
+        "query": json.dumps(query),
+        "statusCode": 200,
     }
--- src/config.py
+++ src/config.py
@@ -1,9 +1,57 @@
 import os
+from pathlib import Path
+
+from transformers import (
+    T5Config,
+    T5ForConditionalGeneration,
+    T5Tokenizer,
+    AutoTokenizer,
+    AutoModelForSeq2SeqLM,
+)
+
+from fastT5 import (
+    export_and_get_onnx_model,
+    generate_onnx_representation,
+    quantize,
+    get_onnx_model,
+    get_onnx_runtime_sessions,
+    OnnxT5,
+)
 
 # default config
 class Config(object):
     REGION = "eu-west-1"
     # memcache local address
-    MEMCACHE_ADDRESS = (
-        "faiss-based-sentence-similarity-retrieval.y9tsob.cfg.euw1.cache.amazonaws.com"
+    MODEL_DIR = "./model/"
+    
+    
+def onnx_model_init(qg_model):
+    
+    # print(Path(qg_model).stem)
+    encoder_path = os.path.join(qg_model, f"onnx/{Path(qg_model).stem}-encoder-quantized.onnx")
+    decoder_path = os.path.join(qg_model, f"onnx/{Path(qg_model).stem}-decoder-quantized.onnx")
+    init_decoder_path = os.path.join(qg_model, f"onnx/{Path(qg_model).stem}-init-decoder-quantized.onnx")
+    tokenizer_path = os.path.join(qg_model,"onnx")
+    # print(encoder_path)
+    
+    model_paths = encoder_path, decoder_path, init_decoder_path
+
+    model_sessions = get_onnx_runtime_sessions(model_paths)
+    model = OnnxT5(qg_model, model_sessions)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+    
+    return model, tokenizer
+
+
+def generate_question(user_utterance, model, tokenizer):
+    
+    input_ids = tokenizer.encode(user_utterance, return_tensors="pt")
+    outputs = model.generate(
+        input_ids=input_ids,
+        max_length=64,
+        do_sample=True,
+        top_p=0.95,
+        num_return_sequences=3,
     )
+    
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
